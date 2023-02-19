e221d087b29b348ef4e9e776963ac2c28cfd9cb0
1toldyou <86680163+1toldyou@users.noreply.github.com>
2023-02-19 13:08:53 -0800

    wrapper for S3 download

--- src/helper/s3.rs
+++ src/helper/s3.rs
@@ -49,6 +49,48 @@ pub fn upload_files(remote_config: &config_file::Remote, files: &Vec<config_file
         });
     }
 
-
     Ok(uploaded_files)
+}
+
+pub fn download_files(remote_config: &config_file::Remote, files: Vec<&config_file::TargetFile>) -> Result<(), Box<dyn Error>> {
+    if !remote_config.type_.eq("S3") {
+        Err("This function only supports S3")?;
+    }
+
+    let bucket = s3::Bucket::new(
+        &remote_config.bucket_name,
+        s3::Region::Custom {
+            region: remote_config.bucket_region.to_string(),
+            endpoint: remote_config.url.to_string(),
+        },
+        awscreds::Credentials::new(
+            Some(remote_config.access_key.as_str()),
+            Some(remote_config.secret_key.as_str()),
+            None,
+            None,
+            None
+        )?,
+    ).expect("failed to create bucket");
+
+    debug!("Bucket: {:?}", bucket);
+
+    for file in files {
+        debug!("File: {:?}", file);
+        let new_key = format!("{}{}", remote_config.base_dir, file.key);
+        debug!("Remote Path: {}", new_key);
+        let object_response: s3::request_trait::ResponseData = bucket.get_object(&new_key)?;
+
+        debug!("Status: {}", object_response.status_code());
+
+        if !file.directory.is_empty() {
+            debug!("Creating Directory: {}", file.directory);
+            fs::create_dir_all(&file.directory)?;
+        }
+
+        let file_path = format!("{}{}", file.directory, file.filename);
+        info!("File Path: {}", file_path);
+        fs::write(file_path, object_response.bytes().to_vec())?;
+    }
+
+    Ok(())
 }
